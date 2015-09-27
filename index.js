bd9c83ba17293db04d77ccf920a0a6d30a02818c
007lva <lvargas@rankia.com>
2015-09-27 13:19:27 +0200

    Playing with standard

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ const request = require('request')
 const fs = require('fs')
 const baseUrl = 'http://www.axesor.es/directorio-informacion-empresas'
 
-//const req = request.defaults({ baseUrl: baseUrl })
+// const req = request.defaults({ baseUrl: baseUrl })
 const linksEmpresas = []
 
-function doRequest(options) {
+function doRequest (options) {
   return new Promise(
-    function(resolve, reject) {
-      request(options, function(err, response, body) {
+    function (resolve, reject) {
+      request(options, function (err, response, body) {
         if (err) {
           reject(err)
         } else {
@@ -22,27 +22,27 @@ function doRequest(options) {
   )
 }
 
-function doAnalizeMunicipiosLinks(linksMunicipio) {
+function doAnalizeMunicipiosLinks (linksMunicipio) {
   return new Promise(
-    function(resolve, reject) {
+    function (resolve, reject) {
       analizeMunicipiosLinks(linksMunicipio, resolve, reject)
     }
   )
 }
 
-function doAnalizeLinkEmpresasByMunicipio(linksMunicipio) {
+function doAnalizeLinkEmpresasByMunicipio (linksMunicipio) {
   return new Promise(
-    function(resolve, reject) {
+    function (resolve, reject) {
       getLinksEmpresaByMunicipio(linksMunicipio, resolve, reject)
     }
   )
 }
 
-function analizeMunicipiosLinks(linksMunicipio, resolve, reject) {
+function analizeMunicipiosLinks (linksMunicipio, resolve, reject) {
   console.log(`municipios: ${ linksMunicipio.length }`)
   if (linksMunicipio.length > 0) {
     doAnalizeLinkEmpresasByMunicipio(linksMunicipio.shift())
-    .then(function(data) {
+    .then(function (data) {
       analizeMunicipiosLinks(linksMunicipio, resolve, reject)
     })
   } else {
@@ -50,26 +50,26 @@ function analizeMunicipiosLinks(linksMunicipio, resolve, reject) {
   }
 }
 
-function doAnalizeProvinciasLinks(linksProvincia) {
+function doAnalizeProvinciasLinks (linksProvincia) {
   return new Promise(
-    function(resolve, reject) {
+    function (resolve, reject) {
       analizeProvinciasLinks(linksProvincia, [], resolve, reject)
     }
   )
 }
 
-function analizeProvinciasLinks(linksProvincia, linksMunicipio, resolve, reject) {
+function analizeProvinciasLinks (linksProvincia, linksMunicipio, resolve, reject) {
   console.log(`provincias: ${ linksProvincia.length }`)
   if (linksProvincia.length > 0) {
     doRequest({ uri: linksProvincia.shift() })
     .then(function (result) {
       const $ = cheerio.load(result.body)
-      $('.lista3 li').each(function(index, elem) {
+      $('.lista3 li').each(function (index, elem) {
         linksMunicipio.push($(elem).find('a').attr('href'))
       })
       analizeProvinciasLinks(linksProvincia, linksMunicipio, resolve, reject)
     })
-    .catch(function(err) {
+    .catch(function (err) {
       console.log(`analizeProvinciasLinks error: ${ JSON.stringify(err) }`)
     })
   } else {
@@ -77,11 +77,11 @@ function analizeProvinciasLinks(linksProvincia, linksMunicipio, resolve, reject)
   }
 }
 
-function getLinksEmpresaByMunicipio(link, resolve, reject) {
+function getLinksEmpresaByMunicipio (link, resolve, reject) {
   doRequest({ uri: link })
-  .then(function(result) {
+  .then(function (result) {
     const $ = cheerio.load(result.body)
-    $('.t01 tbody tr').each(function(index, elem) {
+    $('.t01 tbody tr').each(function (index, elem) {
       const empresa = decodeRowEmpresa($(elem))
       const csvRow = `${ empresa.url },${ empresa.name },${ empresa.poblacion },${ empresa.provincia }\n`
       fs.appendFileSync('empresas.txt', csvRow, 'utf8')
@@ -93,17 +93,17 @@ function getLinksEmpresaByMunicipio(link, resolve, reject) {
       resolve('Done! -> getLinksEmpresaByMunicipio')
     }
   })
-  .catch(function(err) {
+  .catch(function (err) {
     console.log(`getLinksEmpresaByMunicipio error: ${ JSON.stringify(err) }`)
   })
 }
 
-function nextPageEmpresa(url) {
-  const pageNumber = parseInt(url.substr(-1)) + 1
+function nextPageEmpresa (url) {
+  const pageNumber = parseInt(url.substr(-1), 10) + 1
   return `${ url.substring(0, url.length - 1) }${ pageNumber }`
 }
 
-function decodeRowEmpresa(tr) {
+function decodeRowEmpresa (tr) {
   return {
     url: tr.find('a').attr('href'),
     name: tr.find('.first').text().trim(),
@@ -113,21 +113,21 @@ function decodeRowEmpresa(tr) {
 }
 
 doRequest({ uri: baseUrl })
-.then(function(result) {
+.then(function (result) {
   const $ = cheerio.load(result.body)
   const linksProvincia = []
-  $('.lista3 li').each(function(index, elem) {
+  $('.lista3 li').each(function (index, elem) {
     linksProvincia.push($(elem).find('a').attr('href'))
   })
   return doAnalizeProvinciasLinks(linksProvincia)
 })
-.then(function(linksMunicipio) {
+.then(function (linksMunicipio) {
   return doAnalizeMunicipiosLinks(linksMunicipio)
 })
 .then(function () {
   console.log('FINISH!')
 })
-.catch(function(err) {
+.catch(function (err) {
   console.log(`Error: ${ JSON.stringify(err) }`)
 })
 
